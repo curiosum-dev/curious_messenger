60c47d5d9583a29b89e9e6aa42b03cf2a2402d02
Michał Buszkiewicz <buszkiewiczm@gmail.com>
2019-11-07 10:43:04 +0100

    Add missing conversation functions

--- lib/curious_messenger/chat.ex
+++ lib/curious_messenger/chat.ex
@@ -485,4 +485,100 @@ defmodule CuriousMessenger.Chat do
   def change_conversation_member(%ConversationMember{} = conversation_member) do
     ConversationMember.changeset(conversation_member, %{})
   end
+
+  alias CuriousMessenger.Chat.Conversation
+
+  @doc """
+  Returns the list of chat_conversations.
+
+  ## Examples
+
+      iex> list_chat_conversations()
+      [%Conversation{}, ...]
+
+  """
+  def list_chat_conversations do
+    Repo.all(Conversation)
+  end
+
+  @doc """
+  Gets a single conversation.
+
+  Raises `Ecto.NoResultsError` if the Conversation does not exist.
+
+  ## Examples
+
+      iex> get_conversation!(123)
+      %Conversation{}
+
+      iex> get_conversation!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_conversation!(id), do: Repo.get!(Conversation, id)
+
+  @doc """
+  Creates a conversation.
+
+  ## Examples
+
+      iex> create_conversation(%{field: value})
+      {:ok, %Conversation{}}
+
+      iex> create_conversation(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_conversation(attrs \\ %{}) do
+    %Conversation{}
+    |> Conversation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a conversation.
+
+  ## Examples
+
+      iex> update_conversation(conversation, %{field: new_value})
+      {:ok, %Conversation{}}
+
+      iex> update_conversation(conversation, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_conversation(%Conversation{} = conversation, attrs) do
+    conversation
+    |> Conversation.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Conversation.
+
+  ## Examples
+
+      iex> delete_conversation(conversation)
+      {:ok, %Conversation{}}
+
+      iex> delete_conversation(conversation)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_conversation(%Conversation{} = conversation) do
+    Repo.delete(conversation)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking conversation changes.
+
+  ## Examples
+
+      iex> change_conversation(conversation)
+      %Ecto.Changeset{source: %Conversation{}}
+
+  """
+  def change_conversation(%Conversation{} = conversation) do
+    Conversation.changeset(conversation, %{})
+  end
 end
