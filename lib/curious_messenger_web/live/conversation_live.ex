46407cb385197513b7ee489128236defe24cfeee
Michał Buszkiewicz <buszkiewiczm@gmail.com>
2019-11-12 21:21:43 +0100

    Separate conversation template into .leex file

--- lib/curious_messenger_web/live/conversation_live.ex
+++ lib/curious_messenger_web/live/conversation_live.ex
@@ -5,31 +5,10 @@ defmodule CuriousMessengerWeb.ConversationLive do
   use Phoenix.HTML
 
   alias CuriousMessenger.{Auth, Chat, Repo}
+  alias CuriousMessengerWeb.ConversationView
 
   def render(assigns) do
-    ~L"""
-    <div>
-      <b>User name:</b> <%= @user.nickname %>
-    </div>
-    <div>
-      <b>Conversation title:</b> <%= @conversation.title %>
-    </div>
-    <div>
-      <%= f = form_for :message, "#", [phx_submit: "send_message"] %>
-        <%= label f, :content %>
-        <%= text_input f, :content %>
-        <%= submit "Send" %>
-      </form>
-    </div>
-    <div>
-      <b>Messages:</b>
-      <%= for message <- @messages do %>
-        <div>
-          <b><%= message.user.nickname %></b>: <%= message.content %>
-        </div>
-      <% end %>
-    </div>
-    """
+    ConversationView.render("show.html", assigns)
   end
 
   def mount(assigns, socket) do
